9b02fdb4724c377f3ae0e54c3aa2219bc294f076
CiaShangLin <nick034po@yahoo.com.tw>
2018-08-12 17:46:38 +0800

    取得檔案名稱,修改時間,路徑

--- app/src/main/java/com/shang/mediaplayerbykotlin/MainActivity.kt
+++ app/src/main/java/com/shang/mediaplayerbykotlin/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.shang.mediaplayerbykotlin
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.support.annotation.RequiresApi
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
+import java.sql.Date
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
+import java.util.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,62 +20,75 @@ class MainActivity : AppCompatActivity() {
     var MyLocal_Music_path: String = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).toString()
     var MySDcard_Music_path: String = "/storage/9016-4EF8/"
 
+    var musicList:MutableList<File> = mutableListOf()
+
     var file = Environment.getExternalStorageDirectory()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var publicPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString()
-        var privatePath = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).toString()
-
-
         var bPlay = false
         button.setOnClickListener {
-            while (true) {
-                Log.d("TAG", "path:" + file.path)
-                if (file.listFiles() != null) {
-                    for (item in file.listFiles()) {
-                        var f: String
-                        if (item.isFile) {
-                            f = "File"
-                        } else {
-                            f = "Directory"
-                        }
-                        Log.d("TAG", "${item.name} :is $f")
-                    }
+            Log.d("Music",musicList.size.toString())
+            musicList.forEach {
+                Log.d("Music","Name:"+it.nameWithoutExtension)  //取得沒有附檔名的檔名
+                Log.d("Music","Path:"+it.canonicalPath)          //路徑
+                var s=SimpleDateFormat("yyyy/MM/dd hh:mm:ss").format(Date(it.lastModified()))
 
-                }
-                Log.d("TAG", "----------------------------------------")
+                Log.d("Music","modi:"+s)  //修改時間
+                //播放長度 好像要在MediaPlayer尋找 您可以使用MediaMetadataRetriever獲取歌曲的持續時間。將METADATA_KEY_DURATION與extractMetadata（）函數結合使用。
 
-                try {
-                    file = File(file.parent.toString())
-                } catch (e: Exception) {
-                    break;
-                }
 
             }
 
-
         }
 
-        var local_music = File(MyLocal_Music_path)
-        var sdcard_music = File(MySDcard_Music_path)
+        Find_Music(MyLocal_Music_path)
+        Find_Music(MySDcard_Music_path)
 
-        local_music.walk().filter {
-            it.isFile and (it.extension=="mp3")
-        }.forEach {
-            Log.d("Music",it.name)
-        }
 
-        Log.d("Music","---------------------------------------")
-        sdcard_music.walk().filter {
-            it.isFile and (it.extension=="mp3")
-        }.forEach {
-            Log.d("Music",it.name)
-        }
+
 
     }
+
+    fun Find_All_Directory_Parent(){
+        while (true) {
+            Log.d("TAG", "path:" + file.path)
+            if (file.listFiles() != null) {
+                for (item in file.listFiles()) {
+                    var f: String
+                    if (item.isFile) {
+                        f = "File"
+                    } else {
+                        f = "Directory"
+                    }
+                    Log.d("TAG", "${item.name} :is $f")
+                }
+
+            }
+            Log.d("TAG", "----------------------------------------")
+
+            try {
+                file = File(file.parent.toString())
+            } catch (e: Exception) {
+                break;
+            }
+
+        }
+    }
+
+    fun Find_Music(path:String){
+        var file_music=File(path)
+
+        file_music.walk().filter {
+            it.isFile and (it.extension=="mp3")
+        }.forEach {
+            musicList.add(it)
+        }
+    }
+
 
 
 }
