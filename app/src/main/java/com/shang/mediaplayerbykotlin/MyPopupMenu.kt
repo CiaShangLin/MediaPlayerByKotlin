3ec8dbad51b54d260bff3ce3465f2834fbd4379b
fcu-d0514534 <D0514534@gmail.com>
2019-06-09 23:00:29 +0800

    初始化跟Menu的click處理完成 setting也會更新

--- app/src/main/java/com/shang/mediaplayerbykotlin/MyPopupMenu.kt
+++ app/src/main/java/com/shang/mediaplayerbykotlin/MyPopupMenu.kt
@@ -1,22 +1,58 @@
 package com.shang.mediaplayerbykotlin
 
 import android.content.Context
+import android.content.Entity
+import android.util.Log
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.PopupMenu
+import androidx.lifecycle.Observer
+import com.shang.mediaplayerbykotlin.Room.Setting_Entity
+import com.shang.mediaplayerbykotlin.ViewModel.MediaPlayerModel
 
-class MyPopupMenu: PopupMenu,PopupMenu.OnMenuItemClickListener {
+class MyPopupMenu : PopupMenu, PopupMenu.OnMenuItemClickListener {
 
-    constructor(context: Context, view: View,menuRes:Int):super(context,view){
+    private val TAG:String="MyPopupMenu"
+    private lateinit var mMediaPlayerModel: MediaPlayerModel
+    private var settingEntity: Setting_Entity? = null
+
+    constructor(context: Context, view: View, menuRes: Int, mediaPlayerModel: MediaPlayerModel) : super(context, view) {
         super.inflate(menuRes)
+        this.mMediaPlayerModel = mediaPlayerModel
+        this.settingEntity = mMediaPlayerModel.getSettingNow()
+
+        //初始化
+        Log.v(TAG,"mode:${settingEntity?.sort_mode} type:${settingEntity?.sort_type}" )
+        if (settingEntity != null) {
+            menu.findItem(R.id.sort_mode).setChecked(settingEntity?.sort_mode!!)
+            menu.getItem(settingEntity?.sort_type!!).setChecked(true)
+        } else {
+            mMediaPlayerModel.insertSetting(Setting_Entity())
+        }
     }
 
 
-    override fun onMenuItemClick(p0: MenuItem?): Boolean {
+    override fun onMenuItemClick(p0: MenuItem): Boolean {
+        var setting_Entity = mMediaPlayerModel.getSettingNow()
+        when (p0.itemId) {
+            R.id.sort_mode -> {
+                setting_Entity.sort_mode = !p0.isChecked
+            }
+            R.id.sort_modify -> {
+                setting_Entity.sort_type = 1
+            }
+            R.id.sort_name -> {
+                setting_Entity.sort_type = 2
+            }
+            R.id.sort_time -> {
+                setting_Entity.sort_type = 3
+            }
+        }
+        Log.v(TAG,"mode:${setting_Entity.sort_mode} type:${setting_Entity.sort_type}" )
+        mMediaPlayerModel.updateSetting(setting_Entity) //更新
         return true
     }
-
 
 
 }
